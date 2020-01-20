d23d7099601fe84a281b031683171921020f3b45
Shashank Shekhar Upadhyay <shashankshek.upadhyay@gmail.com>
2020-01-21 00:05:27 +0530

    Initialise typescrpt and chapter 2

---
+++ Chapter2/src/index.ts
@@ -0,0 +1,14 @@
+console.log('Hello TypeScript');
+
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+    apple: a,
+    banana: b
+}
+let d = c.apple * 4;
+
+console.log("a: ", a);
+console.log("b: ", b);
+console.log("c: ", c);
+console.log("d: ", d);
